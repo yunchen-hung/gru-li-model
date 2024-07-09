4f839ca1a1fc8a237949cc976d047c37be543db4
moufan <veritaria@outlook.com>
2024-07-09 02:12:02 -0400

    merged stashed changes

---
+++ analysis/decoding/svm.py
@@ -0,0 +1,85 @@
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+
+from utils import savefig
+
+
+class SVM:
+    def __init__(self, n_splits=5):
+        self.n_splits = n_splits
+        self.results = None
+
+    def fit(self, all_data, gts, mask=None):
+        """
+        data: features, list, can have multiple groups of data, time * context_num * state_dim
+        gts: ground truths, list, each item is a vector representing a group of labels, 
+             can have multiple groups of labels, time * context_num
+        mask: same shape as gts, specifying which data points are valid
+        return: decoding accuracy of each group of labels
+        """
+        results = []
+        for i in range(all_data.shape[0]):
+            data = all_data[i]
+            data_accuracy = []
+            for j in range(gts.shape[0]):
+                gt = gts[j]
+                kf = KFold(n_splits=self.n_splits, shuffle=True)
+                decode_accuracy = 0.0
+                for train_index, test_index in kf.split(data):
+                    data_train, data_test = data[train_index], data[test_index]
+                    gt_train, gt_test = gt[train_index], gt[test_index]
+                    decoder = svm.SVC(decision_function_shape='ovo')
+                    decoder.fit(data_train, gt_train)
+                    pred = decoder.predict(data_test)
+                    decode_accuracy += np.sum(pred == gt_test) / gt_test.shape[0]
+                decode_accuracy /= self.n_splits
+                data_accuracy.append(decode_accuracy)
+            results.append(data_accuracy)
+        self.results = np.array(results)
+        return results
+
+    def visualize(self, save_path, pdf=False):
+        if self.results is None:
+            raise Exception("Please run fit() first")
+        plt.figure(figsize=(1.0 * self.results.shape[0], 4.0), dpi=180)
+        for i, result in enumerate(self.results):
+            plt.plot(np.arange(1, self.results.shape[1]+1), result, label="timestep {}".format(i+1))
+        plt.legend()
+        plt.xlim(0.5, 5.5)
+        plt.title("SVM decoding accuracy")
+        plt.xlabel("memories")
+        plt.ylabel("decoding accuracy")
+
+        ax = plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+        plt.tight_layout()
+        if save_path is not None:
+            savefig(save_path, "svm_accuracy", pdf=pdf)
+
+    def visualize_by_memory(self, save_path, title=None, pdf=False):
+        if self.results is None:
+            raise Exception("Please run fit() first")
+        plt.figure(figsize=(1.0 * self.results.shape[1], 5.0), dpi=180)
+        for i in range(self.results.shape[1]):
+            plt.plot(np.arange(1, self.results.shape[0]+1), self.results[:, i], label="item {}".format(i+1))
+        plt.legend(fontsize=10)
+        plt.xlim(0.5, 0.5 + self.results.shape[0])
+        if title:
+            plt.title(title)
+        plt.xlabel("timesteps")
+        plt.ylabel("decoding accuracy")
+
+        ax = plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+        if title:
+            plt.title(title)
+
+        plt.tight_layout()
+        if save_path is not None:
+            savefig(save_path, "svm_accuracy_by_memory", pdf=pdf)
