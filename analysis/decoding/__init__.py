b4128a83b65fbb627537916b2c369cadf91a48d4
moufanli <moufan.li@nyu.edu>
2024-08-20 18:07:43 -0400

    added analysis of strategies

--- analysis/decoding/__init__.py
+++ analysis/decoding/__init__.py
@@ -3,3 +3,4 @@ from .item_index import ItemIndexDecoder
 from .single_neuron_selectivity import SingleNeuronSelectivity
 from .pc_selectivity import PCSelectivity
 from .dm_answer import DMAnserDecoder
+from .regression import Regressor
