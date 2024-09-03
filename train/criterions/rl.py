fixed bugs in rl loss masking

train/criterions/rl.py
@@ -55,7 +55,7 @@ class A2CLoss(nn.Module):
         values = torch.stack(values).squeeze(2).to(device).transpose(1, 0)
         returns = returns.to(device)
         # entropys = torch.stack(entropys).to(device)
-        entropys = torch.stack([torch.stack(entropys_t) for entropys_t in entropys])
+        entropys = torch.stack([torch.stack(entropys_t) for entropys_t in entropys]).transpose(1, 0)
 
         if loss_masks is None:
             loss_masks = torch.ones_like(returns)
@@ -63,12 +63,6 @@ class A2CLoss(nn.Module):
             loss_masks = torch.tensor(loss_masks).to(device).transpose(1, 0)
         
         batch_size = probs.shape[0]
-
-        # probs, values, rewards, entropys = torch.stack([torch.stack(prob_t).to(device) for prob_t in probs]).transpose(1, 0).to(device), \
-        #                         torch.stack(values).squeeze(2).transpose(1, 0).to(device), \
-        #                         returns.to(device), \
-        #                         torch.stack(entropys)
-                                #torch.stack([torch.stack(entropys_t) for entropys_t in entropys])
 
         if print_info:
             print("loss info:", probs[0], values[0], returns[0])
@@ -78,22 +72,18 @@ class A2CLoss(nn.Module):
             if self.value_loss_func == 'mse':
                 value_losses = 0.5 * mse_loss(torch.squeeze(values.to(device).float()), torch.squeeze(returns.to(device).float()))
             elif self.value_loss_func == 'l1':
-                value_losses = smooth_l1_loss(torch.squeeze(values.to(device).float()), torch.squeeze(returns.to(device).float()))
+                value_losses = smooth_l1_loss(torch.squeeze(values[loss_masks != 0].to(device).float()), 
+                                              torch.squeeze(returns[loss_masks != 0].to(device).float())) * values[loss_masks != 0].shape[0]
             # smooth_l1_loss(torch.squeeze(v_t.to(self.device)), torch.squeeze(R_t.to(self.device)))
         else:
             # policy gradient loss
             A = returns
             value_losses = torch.tensor(0.0).to(device)
         # accumulate policy gradient
-        # print(loss_masks, probs, A)
-        # print(probs[loss_masks != 0], A[loss_masks != 0])
         policy_grads = -probs[loss_masks != 0] * A[loss_masks != 0]
         policy_gradient = torch.sum(policy_grads) / batch_size
         value_loss = torch.sum(value_losses) / batch_size
-        pi_ent = torch.sum(entropys) / batch_size
-        # policy_gradient = torch.mean(policy_grads)
-        # value_loss = torch.mean(value_losses)
-        # pi_ent = torch.mean(entropys)
+        pi_ent = torch.sum(entropys[loss_masks != 0]) / batch_size
         loss = policy_gradient + value_loss - pi_ent * self.eta
         return loss, policy_gradient, value_loss, pi_ent * self.eta
 

train/train.py
@@ -90,6 +90,7 @@ def train(agent, envs, optimizer, scheduler, criterion, sl_criterion,
         correct_actions, wrong_actions, not_know_actions = 0, 0, 0
 
         # reset environment
+        print(1)
         obs_, info = env.reset()
         obs = torch.Tensor(obs_).to(device)
         # print(env.memory_sequence)
@@ -134,26 +135,6 @@ def train(agent, envs, optimizer, scheduler, criterion, sl_criterion,
                     correct_actions += np.sum(info["correct"])
                     wrong_actions += np.sum(info["wrong"])
                     not_know_actions += np.sum(info["not_know"])
-                    # if done.all():
-                    #     gts.append([final_info["gt"] for final_info in info["final_info"]])
-                    #     gt_masks.append([final_info["gt_mask"] for final_info in info["final_info"]])
-                    #     loss_mask = np.array([final_info["loss_mask"] for final_info in info["final_info"]])
-                    #     print(loss_mask)
-                    #     loss_masks.append(np.logical_and(loss_mask, np.logical_not(terminated)))
-                    #     correct_actions += np.sum([final_info["correct"] for final_info in info["final_info"]])
-                    #     wrong_actions += np.sum([final_info["wrong"] for final_info in info["final_info"]])
-                    #     not_know_actions += np.sum([final_info["not_know"] for final_info in info["final_info"]])
-                    # else:
-                    #     gts.append(info["gt"])
-                    #     gt_masks.append(info["gt_mask"])
-                    #     loss_mask = np.array(info["loss_mask"])
-                    #     print(info)
-                    #     print(loss_mask)
-                    #     loss_masks.append(np.logical_and(loss_mask, np.logical_not(terminated)))
-                    #     correct_actions += np.sum(info["correct"])
-                    #     wrong_actions += np.sum(info["wrong"])
-                    #     not_know_actions += np.sum(info["not_know"])
-                    # print()
                     terminated = np.logical_or(terminated, done)
                 outputs[j].append(o)
                 probs[j].append(log_prob_action)