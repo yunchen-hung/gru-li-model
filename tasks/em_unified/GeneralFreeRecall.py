e648851f6758817b06f8b676d56f205e8a321243
moufanli <moufan.li@nyu.edu>
2025-02-01 21:36:41 -0500

    modified unified EM task set

--- tasks/em_unified/GeneralFreeRecall.py
+++ tasks/em_unified/GeneralFreeRecall.py
@@ -17,13 +17,42 @@ class GeneralFreeRecall(BaseGeneralEMTask):
         self._generate_task_condition()
         return obs, info
 
+    
+    def step(self, action):
+        obs, reward, done, info = super().step(action)
+        if self.phase == "recall":
+            reward, correct, wrong, not_know = self._compute_reward_and_update_metrics(action)
+            info["correct"] = correct
+            info["wrong"] = wrong
+            info["not_know"] = not_know
+            info["done"] = done
+        return obs, reward, done, False, info
 
-    def _generate_condition_features(self):
+
         self.condition_feature = None
         self.condition_value = None
         self.query_feature = None
         self.answer = None
         self.num_matched_item = self.sequence_len
 
+    
+    def _compute_reward_and_update_metrics(self, action):
+        action_item = self._convert_action_to_item(action)
+        correct, wrong, not_know = 0, 0, 0
+        if action_item in self.memory_sequence:
+            correct += 1
+            reward = self.correct_reward
+        elif action_item == -1:
+            not_know += 1
+            reward = self.no_action_reward
+        else:
+            wrong += 1
+            reward = self.wrong_reward
+        return reward, correct, wrong, not_know
 
 
+    def _generate_gt(self):
+        if self.action_space_type == "feature_wise":
+            return self.memory_sequence[self.timestep]
+        elif self.action_space_type == "task_wise":
+            return self._convert_item_to_int(self.memory_sequence[self.timestep])
--- tasks/em_unified/base.py
+++ tasks/em_unified/base.py
@@ -22,12 +22,13 @@ class BaseGeneralEMTask(BaseEMTask):
                  include_question_during_encode=False,  # whether to give the condition/question during encode
                  question_type="sum",                   # question_type: sum, xor
                  sum_reference=1,                       # sum_reference: the value to compare the sum with
-                 answer_dim=3,                         # answer_dim: the dimension of the answer space
+                 answer_dim=2,                         # answer_dim: the dimension of the answer space
                  
                  one_hot_stimuli=True,
-                 action_space_type="feature_wise",      # action_space_type: feature_wise (an action for each feature), task_wise (an action for each task)
+                 action_space_type="task_wise",      # action_space_type: feature_wise (an action for each feature), task_wise (an action for each task)
 
-                 seed=None):
+                 seed=None,
+                 **kwargs):
         super().__init__(reset_state_before_test=reset_state_before_test, seed=seed)
         self.num_features = num_features
         self.feature_dim = feature_dim
@@ -54,9 +55,9 @@ class BaseGeneralEMTask(BaseEMTask):
 
         action_space_dim = 0
         if self.action_space_type == "feature_wise":
-            action_space_dim = self.num_features * self.feature_dim + self.answer_dim
+            action_space_dim = self.num_features * self.feature_dim + self.answer_dim + 1 # +1 for no action
         elif self.action_space_type == "task_wise":
-            action_space_dim = self.feature_dim ** self.num_features + self.feature_dim + self.answer_dim # free recall, recall feature, answer question
+            action_space_dim = self.feature_dim ** self.num_features + 1 + self.feature_dim + 1 + self.answer_dim + 1 # free recall, recall feature, answer question
 
         self.action_space = spaces.Box(low=0, high=1, shape=(action_space_dim,), dtype=np.float32)
 
@@ -66,59 +67,9 @@ class BaseGeneralEMTask(BaseEMTask):
     def reset(self):
         self.memory_sequence = self.all_stimuli[np.random.choice(len(self.all_stimuli), self.sequence_len, replace=True)]
 
-        if self.task_type == "free_recall":
-            self.condition_feature = None
-            self.condition_value = None
-            self.query_feature = None
-        elif self.task_type == "cond_free_recall":
-            iter_num = 0
-            while True:
-                self.condition_feature = np.random.choice(self.num_features)
-                self.condition_value = np.random.choice(self.feature_dim)
-                if np.sum(self.memory_sequence[:, self.condition_feature] == self.condition_value) > 0 or iter_num>10:
-                    break
-            self.query_feature = None
-        elif self.task_type == "cond_qa" or self.task_type == "cond_free_recall_feature":
-            iter_num = 0
-            while True:
-                rand_feature = np.random.choice(self.num_features, 2, replace=False)
-                self.condition_feature = rand_feature[0]
-                self.condition_value = np.random.choice(self.feature_dim)
-                self.query_feature = rand_feature[1]
-                if np.sum(self.memory_sequence[:, self.condition_feature] == self.condition_value) > 0 or iter_num>10:
-                    break
-        else:
-            raise ValueError(f"task_type {self.task_type} not supported")
-
-        if self.task_type == "cond_qa":
-            self.answer = 0
-            self.cnt = 0
-            for i in range(self.sequence_len):
-                if self.memory_sequence[i, self.query_feature] == self.condition_value:
-                    if self.question_type == "xor":
-                        self.answer = np.logical_xor(self.answer, self.memory_sequence[i, self.condition_feature])
-                    else:
-                        self.answer += self.memory_sequence[i, self.condition_feature]
-                    self.cnt += 1
-            if self.cnt == 0:
-                self.answer = 0
-            else:
-                self.answer = int(self.answer)
-        elif self.task_type == "cond_free_recall_feature" or self.task_type == "cond_free_recall":
-            self.answer = None
-            self.cnt = 0
-            for i in range(self.sequence_len):
-                if self.memory_sequence[i, self.query_feature] == self.condition_value:
-                    self.cnt += 1
-        elif self.task_type == "free_recall":
-            self.answer = None
-            self.cnt = self.sequence_len
-        else:
-            raise ValueError(f"task_type {self.task_type} not supported")
-
         self.phase = "encoding"     # encoding, recall
         self.timestep = 0
-        self.answered = False
+        self.answered = False       # whether all matched items are recalled/the question is answered
 
         # convert the first observation to concatenated one-hot vectors
         obs = self._generate_observation(self.memory_sequence[0], self.condition_feature, self.condition_value, 
@@ -140,8 +91,34 @@ class BaseGeneralEMTask(BaseEMTask):
                     "loss_mask": True, 
                     "correct": 0, "wrong": 0, "not_know": 0,
                     "done": False}
+            if self.timestep >= self.sequence_len:
+                self.phase = "recall"
+                self.timestep = 0
+                info["phase"] = "recall"
+                info["gt"] = self.memory_sequence[self.timestep]
+                info["gt_mask"] = True
+                obs = self._generate_observation(None, self.condition_feature, self.condition_value,
+                                                    self.query_feature, include_question=True)
+                return obs, 0.0, False, False, info
+            else:
+                obs = self._generate_observation(self.memory_sequence[self.timestep], self.condition_feature, self.condition_value,
+                                                self.query_feature, include_question=self.include_question_during_encode)
+                return obs, 0.0, False, False, info
         elif self.phase == "recall":
-            pass
+            obs = self._generate_observation(None, self.condition_feature, self.condition_value, self.query_feature, include_question=True)
+            info = {"phase": "recall",
+                    "gt": self.memory_sequence[self.timestep], "gt_mask": False,
+                    "loss_mask": True,
+                    "correct": 0, "wrong": 0, "not_know": 0,
+                    "done": False}
+            
+            # for different task
+            # compute the reward and the ground truth
+            # update number of correct, wrong, and not_know
+            # check if the trial is done
+
+            done = False
+            return obs, 0.0, done, False, info
 
 
     def _generate_all_stimuli(self):
@@ -180,16 +157,69 @@ class BaseGeneralEMTask(BaseEMTask):
 
     def _convert_item_to_int(self, item):
         """
-        convert the item to an integer
+        convert the item with multiple features to an integer
         item: shape (num_item, num_features)
         """
         return np.sum(item * (self.feature_dim ** np.arange(self.num_features)), axis=1).astype(int)
+
+    
+    def _convert_action_to_item(self, action):
+        """
+        convert the action to the item
+        action: based on the action space type, the action is either a feature-wise action or a task-wise action
+        item: shape (num_features), ignore the answer dimensions
+        """
+        if self.action_space_type == "feature_wise":
+            item = np.zeros(self.num_features)
+            for i in range(self.num_features):
+                # get each feature value from the action
+                item[i] = np.argmax(action[i*self.feature_dim:i*self.feature_dim+self.feature_dim])
+        elif self.action_space_type == "task_wise":
+            item = np.zeros(self.num_features)
+            if action[self.feature_dim**self.num_features] == 1:
+                # considered as no action
+                item = -1
+            else:
+                action_int = np.argmax(action[:self.feature_dim**self.num_features])
+                for i in range(self.num_features):
+                    item[i] = action_int // (self.feature_dim**i) % self.feature_dim
+        return item
+
+    
+    def _convert_action_to_feature(self, item):
+        """
+        convert the action to the query feature value
+        action: based on the action space type, the action is either a feature-wise action or a task-wise action
+        feature: the query feature
+        """
+        if self.action_space_type == "feature_wise":
+            feature = np.argmax(action[self.query_feature*self.feature_dim:self.query_feature*self.feature_dim+self.feature_dim])
+        elif self.action_space_type == "task_wise":
+            feature = np.argmax(action[self.feature_dim**self.num_features:self.feature_dim**self.num_features+self.feature_dim])
+        return feature
+
+    
+    def _convert_action_to_answer(self, action):
+        """
+        convert the action to the answer
+        action: based on the action space type, the action is either a feature-wise action or a task-wise action
+        answer: the answer
+        """
+        answer = np.argmax(action[-self.answer_dim:])
+        return answer
 
 
     def _generate_task_condition(self):
         """
         generate condition_feature, condition_value, query_feature, answer, num_matched_item
         a different method for different task
+        """
+        raise NotImplementedError
+
+    
+    def _compute_reward(self, action):
+        """
+        check if the action is correct, return the reward
         """
         raise NotImplementedError
 
